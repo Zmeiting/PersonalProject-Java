1c5b712b2011ab6c937fee866fa47dfcb880d98e
ZhouMeiTing <1163174105@qq.com>
2021-03-02 16:13:41 +0800

    修改

--- 221801339_zhoumeiting/example/src/WordCount.java
+++ 221801339_zhoumeiting/example/src/WordCount.java
@@ -1,3 +1,6 @@
+package WordCount;
+
+import java.io.File;
 import java.util.*;
 public class WordCount {
 	public static void main(String[] args) {
@@ -7,25 +10,29 @@ public class WordCount {
 		String [] total = file.split("[\\s+]");
 		String inputFile=total[0].toString();
 		String outputFile=total[1].toString();
-		//Test test=new Test();
-		//test.reduceText(inputFile); 测试用代码
-		//Set<String> file_paths = new HashSet<String>();
-		//file_paths.add(file);
-	    //String file_path="E:\\output.txt";  //因为是多个文件输入，一个文件输出，暂时不知道怎么同时输入文件路径
-		//Set<String> file_paths = new HashSet<String>();
-		String characters=new CountUtil().ReturnCharactersNum(inputFile);
-		String words=new CountUtil().ReturnWordsNum(inputFile);
-		String lines=new CountUtil().ReturnLinesNum(inputFile);
-		String maxWords=new CountUtil().ReturnMaxWordsNum(inputFile);
-        String context=characters+words+lines+maxWords;
-		if(new CountUtil().OutputFile(outputFile, context))
+		File input=new File(inputFile);
+		if(input.exists()) //确保输入文件存在
 		{
-			System.out.println("File output success !");
-		}
-		else
-		{
+			//Test test=new Test();
+		    //test.reduceText(inputFile); 测试用代码
+		    //Set<String> file_paths = new HashSet<String>();
+		    //file_paths.add(file);
+	        //String file_path="E:\\output.txt";  //因为是多个文件输入，一个文件输出，暂时不知道怎么同时输入文件路径
+		    //Set<String> file_paths = new HashSet<String>();
+		    String characters=new CountUtil().ReturnCharactersNum(inputFile);
+		    String words=new CountUtil().ReturnWordsNum(inputFile);
+		    String lines=new CountUtil().ReturnLinesNum(inputFile);
+		    String maxWords=new CountUtil().ReturnMaxWordsNum(inputFile);
+            String context=characters+words+lines+maxWords;
+		    if(new CountUtil().OutputFile(outputFile, context))
+		    {
+			    System.out.println("File output success !");
+		    }
+		    else
+		    {
 				System.out.println("error !");
-		}
-		in.close();
+		    }
+		    }
+	    in.close();
 	}
 }
